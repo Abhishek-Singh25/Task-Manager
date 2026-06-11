9278bcb7c1d45554a8224c59bf5b676576feefe9
Abhishek-Singh25 <sabhishek52@yahoo.com>
2026-06-11 22:46:46 +0530

    Update api.js

--- Frontend/taskapp/src/api.js
+++ Frontend/taskapp/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API=axios.create({
-    baseURL: "https://task-manager-backend-p0ad.onrender.com"
+    baseURL: "https://task-manager-backend-p0ad.onrender.com/api"
 });
 
 API.interceptors.request.use((req)=>{
